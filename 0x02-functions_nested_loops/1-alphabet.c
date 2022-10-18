93667393edb0f9e15aaab7a4fe9df6c7bc2b0137
reem-3 <ziyezizu@example.com>
2022-10-18 05:44:17 -0700

    added a function that prints an alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+int j;
+
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
