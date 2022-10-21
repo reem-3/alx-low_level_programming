fe8541ee4d2111457e144a686af2d43ee4aef7a3
reem-3 <ziyezizu@example.com>
2022-10-20 20:15:05 -0700

    use tag not space

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,21 +7,21 @@
  */
 void print_square(int size)
 {
-       if (size <= 0)
-       {
-               _putchar('\n');
-       }
-       else
-       {
-            int row, column;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int row, column;
 
-	    for (row = 0; row < size; row++)
-	     {
-		 for (column = 0; column < size; column++)
-		 {
-		     _putchar('#');
-		 }
-	_putchar('\n');
-}
-}
+		for (row = 0; row < size; row++)
+		{
+			for (column = 0; column < size; column++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
