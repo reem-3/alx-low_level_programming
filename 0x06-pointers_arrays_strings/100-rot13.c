e46f4fc18c822a752acf43ba7668c4fcfd888cb4
reem-3 <ziyezizu@example.com>
2022-11-03 11:24:51 -0700

    encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,12 +8,13 @@
 char *rot13(char *s)
 {
 int count = 0, i;
+
 char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-
 while (*(s + count) != '\0')
 {
 for (i = 0; i < 52; i++)
+{
 if (*(s + count) == alphabet[i])
 {
 *(s + count) = rot13[i];
@@ -22,5 +23,6 @@ break;
 }
 count++;
 }
+
 return (s);
 }
