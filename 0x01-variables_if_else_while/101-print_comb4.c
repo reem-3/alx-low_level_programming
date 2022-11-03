cd1aaf72f6c28011f427826fc985f27d47301e90
reem-3 <ziyezizu@example.com>
2022-11-03 10:00:53 -0700

    prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,12 +5,16 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
 int i, e, g;
+
 i = 48;
 e = 48;
 g = 48;
+
 while (e < 58)
 {
 i = 48;
@@ -20,13 +24,13 @@ g = 48;
 while (g < 58)
 {
 if (e != i && e != g && i != g && e < i && i < g)
-}
+{
 putchar(e);
 putchar(i);
 putchar(g);
 if (i == 56 && e == 55 && g == 57)
-{
-break;
+{	
+break
 }
 putchar(',');
 putchar(' ');
@@ -36,7 +40,6 @@ g++;
 i++;
 }
 e++;
-}
 putchar('\n');
-return(0);
+return (0);
 }
