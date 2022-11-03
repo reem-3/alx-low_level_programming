b53d4c493d046ce7649ad4b56b0fad1467ed80df
reem-3 <ziyezizu@example.com>
2022-11-02 18:08:13 -0700

    compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -26,13 +26,13 @@ int wildcmp(char *s1, char *s2)
           return (*s2 == '\0');
    }
 
-   if (*s2 == '*')
+  if (*s2 == '*')
    {
           return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
    }
-   else if (*s1 == *s2)
+  else if (*s1 == *s2)
    {
           return (wildcmp(s1 + 1, s2 + 1));
    }
-   return (0);
+  (return (0);
 }
