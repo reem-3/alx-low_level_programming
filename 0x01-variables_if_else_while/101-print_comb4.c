f046f7c1204dd2ef913d0938920feece82642751
reem-3 <ziyezizu@example.com>
2022-11-03 10:30:53 -0700

    prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -30,7 +30,7 @@ putchar(i);
 putchar(g);
 if (i == 56 && e == 55 && g == 57)
 {	
-break
+break;
 }
 putchar(',');
 putchar(' ');
@@ -40,6 +40,7 @@ g++;
 i++;
 }
 e++;
+}
 putchar('\n');
 return (0);
 }
